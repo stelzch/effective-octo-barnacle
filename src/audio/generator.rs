97783def145a18676ddec48cf2f9e68b06d9939e
Christoph Stelz <mail@ch-st.de>
2020-04-15 13:25:50 +0200

    Add transforms to polygons and points

--- src/audio/generator.rs
+++ src/audio/generator.rs
@@ -4,6 +4,10 @@ extern crate libpulse_simple_binding as psimple;
 use pulse::stream::Direction;
 use pulse::sample;
 use std::f64::consts::PI;
+use crate::graphics::Polygon;
+use crate::graphics::interpolate_points;
+use std::cmp::max;
+use std::slice;
 
 pub const SAMPLE_RATE : u32 = 44100;
 pub const BIT_DEPTH : u32 = 16;
@@ -36,7 +40,7 @@ pub fn init_audio() -> AudioHandle {
     }
 }
 
-pub fn wavetable_to_data(left : &Vec<i16>, right : &Vec<i16>) -> Vec<u8> {
+pub fn wavetable_to_data(left : &[i16], right : &[i16]) -> Vec<u8> {
     // Left and right channel must contain the same number of samples
     assert_eq!(left.len(), right.len());
 
@@ -79,4 +83,34 @@ pub fn sine_wavetable(freq : u32, phase : f64, amplitude : f64) -> Vec<i16> {
         ((f64::from(t as i32) / f64::from(samples_per_repetition as i32) * 2.0 * PI + phase).sin()
             * 2.0f64.powi(BIT_DEPTH as i32 - 1) * amplitude) as i16
     });
+}
+
+pub fn polygon(num_samples : usize, polygon : &Polygon) -> Vec<u8> {
+    let mut left_channel : Vec<i16> = vec!(0; num_samples);
+    let mut right_channel : Vec<i16> = vec!(0; num_samples);
+
+    let perimeter = polygon.perimeter();
+    let mut starting_index : usize = 0;
+    polygon.iterate_lines_mut(|a, b| {
+        let line_length = a.distance_to(b);
+        let line_samples = (f64::from(line_length) / perimeter * f64::from(num_samples as i32)) as usize;
+        for x in 0..(line_samples) {
+            let t : f64 = f64::from(x as u32) / f64::from(line_samples as i32);
+            let intermediate_point = interpolate_points(a, b, t);
+            let mut idx = starting_index + x;
+
+            if idx >= num_samples {
+                idx = num_samples - 1;
+            }
+
+            left_channel[idx] = (polygon.x + intermediate_point.x * 2.0f64.powi(BIT_DEPTH as i32 -1) * 0.9) as i16;
+            right_channel[idx] = (polygon.y + intermediate_point.y * 2.0f64.powi(BIT_DEPTH as i32 -1) * 0.9) as i16;
+
+        }
+
+        starting_index += line_samples;
+
+    });
+
+    wavetable_to_data(&left_channel[..starting_index], &right_channel[..starting_index])
 }
--- src/graphics.rs
+++ src/graphics.rs
@@ -1,11 +1,99 @@
 pub struct Point2D {
-    x: f64,
-    y: f64
+    pub x: f64,
+    pub y: f64
 }
 
-pub fn interpolate_points(a: Point2D, b: Point2D, t: f64) -> Point2D {
-    let diff_x : f64 = a.x - b.x;
-    let diff_y : f64 = a.y - b.y;
+type Matrix2D = [[f64; 2]; 2];
+
+fn rotation_matrix(angle : f64) -> Matrix2D {
+    let mut m : Matrix2D = [[0.0; 2]; 2];
+    m[0][0] = f64::cos(angle); 
+    m[0][1] = -1.0 * f64::sin(angle);
+    m[1][0] = f64::sin(angle);
+    m[1][1] = f64::cos(angle);
+
+    m
+}
+
+impl Point2D {
+    pub fn distance_to(&self, other : &Point2D) -> f64 {
+        ((self.x - other.x).powi(2) + (self.y - other.y).powi(2)).sqrt()
+    }
+
+    // matrix is in row-major form
+    pub fn transform(&mut self, matrix: &Matrix2D) {
+        let new_x = self.x * matrix[0][0] + self.y * matrix[0][1];
+        let new_y = self.x * matrix[1][0] + self.y * matrix[1][1];
+
+        self.x = new_x;
+        self.y = new_y;
+    }
+
+    pub fn rotate(&mut self, angle : f64) {
+        self.transform(&rotation_matrix(angle));
+    }
+
+    pub fn translate(&mut self, dx : f64, dy : f64) {
+        self.x += dx;
+        self.y += dy;
+    }
+
+    pub fn scale(&mut self, sx : f64, sy : f64) {
+        self.x *= sx;
+        self.y *= sy;
+
+    }
+}
+
+pub struct Polygon {
+    pub points : Vec<Point2D>,
+    pub x : f64,
+    pub y : f64
+}
+
+impl Polygon {
+    pub fn iterate_lines_mut(&self, mut f : impl FnMut(&Point2D, &Point2D)) {
+        if self.points.len() < 2 {
+            panic!("Can't iterate over lines with a polygon that has less than 2 points");
+        }
+
+        let mut last_idx : usize = 0;
+        let n = self.points.len();
+        loop { 
+            f(&self.points[last_idx % n], &self.points[(last_idx + 1) % n]);
+
+            last_idx = (last_idx + 1) % n;
+            if last_idx == 0 {
+                break;
+            }
+        }
+    }
+
+    pub fn perimeter(&self) -> f64 {
+        let mut perimeter = 0.0;
+        self.iterate_lines_mut(|a, b| {
+            perimeter += a.distance_to(b);
+        });
+
+        perimeter
+    }
+
+    pub fn rotate(&mut self, angle : f64) {
+        for point in self.points.iter_mut() {
+            point.rotate(angle);
+        }
+    }
+
+    pub fn scale(&mut self, sx : f64, sy : f64) {
+        for point in self.points.iter_mut() {
+            point.scale(sx, sy);
+        }
+    }
+}
+
+pub fn interpolate_points(a: &Point2D, b: &Point2D, t: f64) -> Point2D {
+    let diff_x : f64 = b.x - a.x;
+    let diff_y : f64 = b.y - a.y;
 
     Point2D {
         x: a.x + (t * diff_x),
