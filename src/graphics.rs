0b221089dae5be30372323826a3faf8c233d23c7
Christoph Stelz <mail@ch-st.de>
2020-04-15 19:44:55 +0200

    draw some rectangles

--- src/graphics.rs
+++ src/graphics.rs
@@ -1,22 +1,46 @@
 pub struct Point2D {
     pub x: f64,
-    pub y: f64
+    pub y: f64,
 }
 
 type Matrix2D = [[f64; 2]; 2];
 
-fn rotation_matrix(angle : f64) -> Matrix2D {
-    let mut m : Matrix2D = [[0.0; 2]; 2];
-    m[0][0] = f64::cos(angle); 
+fn rotation_matrix(angle: f64) -> Matrix2D {
+    let mut m: Matrix2D = [[0.0; 2]; 2];
+    m[0][0] = f64::cos(angle);
     m[0][1] = -1.0 * f64::sin(angle);
     m[1][0] = f64::sin(angle);
     m[1][1] = f64::cos(angle);
 
     m
 }
+pub fn rectangle(x : f64, y : f64, width : f64, height : f64) -> Polygon {
+    Polygon {
+        points: vec![
+            Point2D {
+                x,
+                y
+            },
+            Point2D {
+                x,
+                y: y + height
+            },
+            Point2D {
+                x: x + width,
+                y: y + height
+            },
+            Point2D {
+                x: x + width,
+                y
+            }
+        ],
+        x,
+        y
+    }
+}
 
 impl Point2D {
-    pub fn distance_to(&self, other : &Point2D) -> f64 {
+    pub fn distance_to(&self, other: &Point2D) -> f64 {
         ((self.x - other.x).powi(2) + (self.y - other.y).powi(2)).sqrt()
     }
 
@@ -29,37 +53,36 @@ impl Point2D {
         self.y = new_y;
     }
 
-    pub fn rotate(&mut self, angle : f64) {
+    pub fn rotate(&mut self, angle: f64) {
         self.transform(&rotation_matrix(angle));
     }
 
-    pub fn translate(&mut self, dx : f64, dy : f64) {
+    pub fn translate(&mut self, dx: f64, dy: f64) {
         self.x += dx;
         self.y += dy;
     }
 
-    pub fn scale(&mut self, sx : f64, sy : f64) {
+    pub fn scale(&mut self, sx: f64, sy: f64) {
         self.x *= sx;
         self.y *= sy;
-
     }
 }
 
 pub struct Polygon {
-    pub points : Vec<Point2D>,
-    pub x : f64,
-    pub y : f64
+    pub points: Vec<Point2D>,
+    pub x: f64,
+    pub y: f64,
 }
 
 impl Polygon {
-    pub fn iterate_lines_mut(&self, mut f : impl FnMut(&Point2D, &Point2D)) {
+    pub fn iterate_lines_mut(&self, mut f: impl FnMut(&Point2D, &Point2D)) {
         if self.points.len() < 2 {
             panic!("Can't iterate over lines with a polygon that has less than 2 points");
         }
 
-        let mut last_idx : usize = 0;
+        let mut last_idx: usize = 0;
         let n = self.points.len();
-        loop { 
+        loop {
             f(&self.points[last_idx % n], &self.points[(last_idx + 1) % n]);
 
             last_idx = (last_idx + 1) % n;
@@ -78,25 +101,26 @@ impl Polygon {
         perimeter
     }
 
-    pub fn rotate(&mut self, angle : f64) {
+    pub fn rotate(&mut self, angle: f64) {
         for point in self.points.iter_mut() {
             point.rotate(angle);
         }
     }
 
-    pub fn scale(&mut self, sx : f64, sy : f64) {
+    pub fn scale(&mut self, sx: f64, sy: f64) {
         for point in self.points.iter_mut() {
             point.scale(sx, sy);
         }
     }
+
 }
 
 pub fn interpolate_points(a: &Point2D, b: &Point2D, t: f64) -> Point2D {
-    let diff_x : f64 = b.x - a.x;
-    let diff_y : f64 = b.y - a.y;
+    let diff_x: f64 = b.x - a.x;
+    let diff_y: f64 = b.y - a.y;
 
     Point2D {
         x: a.x + (t * diff_x),
-        y: a.y + (t * diff_y)
+        y: a.y + (t * diff_y),
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -8,15 +8,50 @@ mod graphics;
 
 use audio::generator::AudioHandle;
 
+use graphics::Point2D;
+use graphics::Polygon;
+
 fn main() {
     let audio : AudioHandle = audio::generator::init_audio();
 
     let left_wavetable = audio::generator::sine_wavetable(444, 0.0, 0.2);
-    let right_wavetable = audio::generator::sine_wavetable(444, 0.0, 0.7);
+    let right_wavetable = audio::generator::sine_wavetable(444, 0.25 * PI, 0.7);
 
     let audio_data = audio::generator::wavetable_to_data(&left_wavetable, &right_wavetable);
 
+    let mut triangle = Polygon {
+        points : vec![
+            Point2D {
+                x: -0.5,
+                y: -0.5
+            },
+            Point2D {
+                x: 0.0,
+                y: 0.5
+            },
+            Point2D {
+                x: 0.5,
+                y: -0.5
+            },
+            Point2D {
+                x: 0.0,
+                y: -0.25
+            }
+        ],
+        x: 0.0,
+        y: 0.0
+
+    };
+
+    let mut bounding_rect = graphics::rectangle(-1.0, -1.0, 2.0, 2.0);
+    let mut left_paddle = graphics::rectangle(-0.80, -0.1, 0.2, 0.4);
+    let mut t = 0.0;
+
     loop {
-        audio.pa.write(&audio_data).expect("Failed to write");
+        audio.pa.write(&audio::generator::polygon(200, &left_paddle)).expect("Failed to write");
+        audio.pa.write(&audio::generator::polygon(200, &bounding_rect));
+
+        
+        t += 0.1;
     }
 }
